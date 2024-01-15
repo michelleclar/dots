8a781af8802403a437180f02106e7564dfc0bd2f
Carl <carlmichelle493@gmail.com>
2024-01-15 10:40:19 +0800

    shell

--- sync.sh
+++ sync.sh
@@ -1,67 +1,60 @@
-# rm -rf hypr
-rm -rf kitty
-rm -rf neofetch
-rm -rf nvim
-rm -rf rofi
-rm -rf swappy
-rm -rf swaylock
-rm -rf swww
-rm -rf waybar
-rm -rf wlogout
-rm -rf .zshrc
-rm -rf .vimrc
-rm -rf .zimrc
-rm -rf .ideavimrc
+#!/bin/bash
+# 定义函数
+function sync(){
+  echo "syncing... $1"
 
-# cp -r ~/.config/hypr .
-cp -r ~/.config/kitty .
-cp -r ~/.config/neofetch .
-cp -r ~/.config/nvim .
-cp -r ~/.config/rofi .
-cp -r ~/.config/swappy .
-cp -r ~/.config/swaylock .
-cp -r ~/.config/swww .
-cp -r ~/.config/waybar .
-cp -r ~/.config/wlogout .
-cp -r ~/.zshrc .
-cp -r ~/.vimrc .
-cp -r ~/.zimrc .
-cp -r ~/.ideavimrc .
+  # 使用 rsync 命令合并 rm 和 cp 命令
+  rsync -aq $1 .
 
-function log(){
-   echo "$@" >> .log.log 
-}
-hyper(){
-	echo "syncing...hypr "
-	if sha256sum hypr | grep ~/.config/hypr
-	rm -rf hypr
-	cp -r ~/.config/hypr .
-	echo "sync hypr end"
+  # 检查退出码
+  if [ $? -eq 0 ]; then
+    echo "没有变化，跳过 git 命令"
+  else
+    echo "有变化，执行 git 命令"
 
-	echo "同步到云端"
-	git add .
-	git commit -m "hypr"
-	git push
-}
-function hyper(){
-   echo "syncing...hypr "
-
-   local expected_value=$(sha256sum ~/.config/hypr | cut -d " " -f 1)
-   if sha256sum hypr | grep -q "$expected_value"
-   then
-      log "hypr配置文件已同步"
-   else
-      log "hypr文件校验值不匹配，重新同步"
-      rm -rf hypr
-      cp -r ~/.config/hypr .
-      log "执行命令 
-      rm -rf hypr &&
-      cp -r ~/.config/hypr . "
-      log "同步hypr配置到云端"
-      # 同步到云端
-      git add .
-      git commit -m "hypr"
-      git push
-   fi
+    # 同步到云端
+    git add .
+    git commit -m "$1"
+    git push
+  fi 
+  echo "sync $1 end"
 }
 
+menu(){
+  echo "===================="
+  echo "[1]:sync nvim"
+  echo "[2]:sync hypr"
+  echo "[3]:sync kitty"
+  echo "[4]:sync neofetch"
+  echo "[5]:sync rofi"
+  echo "[6]:sync swappy"
+  echo "[7]:sync swaylock"
+  echo "[8]:sync swww"
+  echo "[9]:sync waybar"
+  echo "[10]:sync wlogout"
+  echo "[11]:sync .zshrc"
+  echo "[12]:sync .vimrc"
+  echo "[13]:sync .zimrc"
+  echo "[14]:sync .ideavimrc"
+  echo "===================="
+}
+menu
+read num
+case $num in
+  1) sync "~/.config/nvim" ;;
+  2) sync "~/.config/hypr" ;;
+  3) sync "~/.config/kitty" ;;
+  4) sync "~/.config/neofetch" ;;
+  5) sync "~/.config/rofi" ;;
+  6) sync "~/.config/swappy" ;;
+  7) sync "~/.config/swaylock" ;;
+  8) sync "~/.config/swww" ;;
+  9) sync "~/.config/waybar" ;;
+  10) sync "~/.config/wlogout" ;;
+  11) sync "~/.zshrc" ;;
+  12) sync "~/.vimrc" ;;
+  13) sync "~/.zimrc" ;;
+  14) sync "~/.ideavimrc" ;;
+  q) echo "quit" ;;
+  *) echo "Invalid option" ;;
+esac;
